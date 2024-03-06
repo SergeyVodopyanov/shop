4b2b073f5b42402702727a473e151e8cb040fa2a
Сергей <vodopyanov03@bk.ru>
2024-03-07 00:11:32 +0300

    Сделал CRUD для категорий

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', \App\Http\Controllers\Main\IndexController::class)->name('main.index');
+Route::get('/admin', \App\Http\Controllers\Main\IndexController::class)->name('main.index');
+
+
+Route::group(['prefix' => 'admin'], function () {
+    Route::group(['prefix' => 'categories'], function () {
+        Route::get('/', [\App\Http\Controllers\Admin\CategoryController::class, 'index'])->name('category.index');
+        Route::get('/create', [\App\Http\Controllers\Admin\CategoryController::class, 'create'])->name('category.create');
+        Route::post('/', [\App\Http\Controllers\Admin\CategoryController::class, 'store'])->name('category.store');
+        Route::get('/{category}', [\App\Http\Controllers\Admin\CategoryController::class, 'show'])->name('category.show');
+        Route::get('/{category}/edit', [\App\Http\Controllers\Admin\CategoryController::class, 'edit'])->name('category.edit');
+        Route::patch('/{category}', [\App\Http\Controllers\Admin\CategoryController::class, 'update'])->name('category.update');
+        Route::delete('/{category}', [\App\Http\Controllers\Admin\CategoryController::class, 'delete'])->name('category.delete');
+    });
+});
+
